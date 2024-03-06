7d589a49fb079a168b3f552471a2d97ca33c1894
Marvin <marvin.ogot@gmail.com>
2024-03-06 23:51:55 +0300

    Countdown lab testing while loops, yet to complete second part of the lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import time
+
+def countdown(num):
+    while num >= 1:
+        print(f"{num} SECONDS()!")
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+countdown(5)
+
+def countdown_with_sleep(num2):
+    while num2 >= 1:
+        print(f"{num2} SECONDS()!")
+        num2 -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
+countdown_with_sleep(8)
+
